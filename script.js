dbffef3a5d03c0b1c922d96b60b09e1d3300f26e
Juan Genova <juang@live.com.ar>
2023-01-31 11:40:31 -0300

    Js commit

--- script.js
+++ script.js
@@ -8,20 +8,36 @@ const operatorButtons = document.querySelectorAll('[data-operator');
 const allClearButton =document.querySelector('[data-all-clear]');
 const deleteButton = document.querySelector('[data-delete]');
 const equalsButton = document.querySelector('[data-equals]');
+
 // constructor
 class Calculator {
     constructor(previousOperandTextElement,currentOperandTextElement) {
-    this.previousOperand = previousOperandTextElement.textContent;
-    this.currentOperand = currentOperandTextElement.textContent; 
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement; 
 
+    this.previousOperand = "";
+    this.currentOperand = "";
+    this.operation = undefined;
     }
     allClear() {
 
     }
     deletDigit() {
-
+        this.currentOperand = this.currentOperand.slice(0,-1)
     }
     appendDigit(digit) {
+        /* Solo pone un punto */
+        if (digit === '.' && this.currentOperand.includes('.')) return;
+
+        /* if (Number.isNaN(Number(this.currentOperand + digit))) return; */
+
+        /*Si pongo un '.' se pone un 0 */
+        if (digit === '.' && this.currentOperand === '')this.currentOperand = '0' + this.currentOperand;
+    
+
+        /* no poner mas de un '0' a la izq. */
+        if(digit === '0' && this.currentOperand.startsWith('0')) return;
+
         this.currentOperand = this.currentOperand + digit;
         console.log(this.currentOperand);
     }
@@ -32,7 +48,7 @@ class Calculator {
 
     }
     updateDisplay() {
-
+        this.currentOperandTextElement.innerText = this.currentOperand;
     }
     getOperandFromDisplay() {
 
@@ -40,15 +56,20 @@ class Calculator {
 
 } /* Class Calculator*/
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
 // event listen
 allClearButton.addEventListener('click', button => {
     calculator.allClear();
+    calculator.updateDisplay();
+})
+deleteButton.addEventListener('click', () =>{
+    calculator.deletDigit();
     calculator.updateDisplay();
 })
 
 numberButtons.forEach(button =>{
     button.addEventListener('click', () => {
         calculator.appendDigit(button.innerText);
-        calculator.updateDisplay;
+        calculator.updateDisplay();
     })
 })
